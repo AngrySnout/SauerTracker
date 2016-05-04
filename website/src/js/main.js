fd34d2e9b6f62600cc122f621437f4dfb9dd7989
AngrySnout <hreudontdy@gmail.com>
2016-05-04 19:32:16 +0300

    Removed unnecessary redundancy.

--- website/src/js/main.js
+++ website/src/js/main.js
@@ -12,16 +12,17 @@ window.moment = moment;
 window.url = url;
 
 window.dateReflow = function() {
+    var now = moment();
     $(".date").each(function (ind, elem) {
         var cd = moment($(this).text()+" +0000"); //, "YYYY-MM-DD HH:mm:ss Z"
         $(this).attr("title", cd.format("dddd, MMMM Do YYYY, HH:mm:ss"));
-        if (cd.diff(moment(), 'hours') >= -22) $(this).text(cd.fromNow());
-        else if (cd.diff(moment(), 'days') >= -7) $(this).text(cd.fromNow()+cd.format(", HH:mm"));
-        else if (!cd.isSame(moment(), 'year')) $(this).text(cd.format("MMMM Do YYYY, HH:mm"));
-        else if (cd.diff(moment(), 'days') < -7) $(this).text(cd.format("MMMM Do, HH:mm"));
+        if (cd.diff(now, 'hours') >= -22) $(this).text(cd.fromNow());
+        else if (cd.diff(now, 'days') >= -7) $(this).text(cd.fromNow()+cd.format(", HH:mm"));
+        else if (!cd.isSame(now, 'year')) $(this).text(cd.format("MMMM Do YYYY, HH:mm"));
+        else if (cd.diff(now, 'days') < -7) $(this).text(cd.format("MMMM Do, HH:mm"));
+        $(this).addClass("date-active");
+        $(this).removeClass("date");
     });
-    $(".date").addClass("date-active");
-    $(".date").removeClass("date");
 };
 
 window.disableDefault = function() {
