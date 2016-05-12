7b4cc0ba8d77bca8ca9873ce5b31226c7297bcfd
AngrySnout <hreudontdy@gmail.com>
2016-05-12 03:20:46 +0300

    Fixed issue after changing the config file schema.

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,12 @@
 var config = require("./tracker.json");
 
+var database = {
+	"client": "pg",
+	"connection": config.databaseConnection,
+	"searchPath": "knex,public"
+};
+
 module.exports = {
-	development: config.database,
-	production: config.database
+	development: database,
+	production: database
 };
