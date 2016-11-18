aaf15814df12053503bb22fde2ba0fa8366f3917
AngrySnout <hreudontdy@gmail.com>
2016-11-18 15:30:16 +0200

    Added team balance API path and UDP server.

--- src/api/v1/players.js
+++ src/api/v1/players.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
+import Promise from 'bluebird';
+import dgram from 'dgram';
+
+import config from '../../../tracker.json';
 
 import app from '../../util/web';
 import database from '../../util/database';
 import playerManager from '../../tracker/player-manager';
+import Packet from '../../util/packet';
 
 export function findPlayers(name, country) {
 	if (typeof name == 'undefined') name = '';
@@ -26,3 +31,122 @@ app.get('/api/players/find', function(req, res) {
 		.then(results => { res.send({ results: results }); })
 		.catch(err => { res.status(500).send({ error: err.message }); });
 });
+
+export function makeTeams(names) {
+	return database.count('* as games').sum('frags as frags').sum('deaths as deaths').sum('flags as flags').select('stats.name').from('stats').join('games', 'games.id', 'stats.game').where('gametype', 'mix').whereRaw("games.timestamp > CURRENT_DATE - INTERVAL '100 months'").whereIn('stats.name', names).whereNot('stats.state', 5).groupBy('stats.name')
+	.then(players => {
+		players = _.keyBy(players, 'name');
+
+		let stats = _.map(names, name => {
+			if (!players[name]) return { name: name, fragginess: 0.5, flagginess: 0.5 };
+			return { name: name, fragginess: players[name].frags*2/players[name].deaths, flagginess: players[name].flags/players[name].games };
+		});
+
+		stats = _.orderBy(stats, ['fragginess', 'flagginess'], ['desc', 'desc']);
+
+		let teams = [{ fragginess: 0, flagginess: 0, players: [] }, { fragginess: 0, flagginess: 0, players: [] }];
+
+		function recalclScore() {
+			for (let j = 0; j < teams.length; j++) {
+				teams[j].fragginess = 0;
+				teams[j].flagginess = 0;
+				for (let i = 0; i < teams[j].players.length; i++) {
+					teams[j].fragginess += teams[j].players[i].fragginess;
+					teams[j].flagginess += teams[j].players[i].flagginess;
+				}
+			}
+		}
+
+		let size = stats.length - (stats.length + 1)%2 - 1;
+		for (let i = 0; i < size; i++) {
+			teams[i%2].players.push(stats[i]);
+		}
+		for (let i = 0; i < stats.length-size; i++) {
+			teams[(i+1)%2].players.push(stats[i + size]);
+		}
+		recalclScore();
+
+		function swapRandom() {
+			let a = Math.floor(Math.random() * teams[0].players.length);
+			let b = Math.floor(Math.random() * teams[1].players.length);
+
+			let tmp = teams[0].players[a];
+			teams[0].players[a] = teams[1].players[b];
+			teams[1].players[b] = tmp;
+			recalclScore();
+		}
+
+		function compareTeamPairs(a, b) {
+			let aDeltaFrag = Math.abs(a[0].fragginess - a[1].fragginess);
+			let bDeltaFrag = Math.abs(b[0].fragginess - b[1].fragginess);
+			let aDeltaFlag = Math.abs(a[0].flagginess - a[1].flagginess);
+			let bDeltaFlag = Math.abs(b[0].flagginess - b[1].flagginess);
+
+			return	aDeltaFrag + aDeltaFlag + Math.abs(aDeltaFrag - aDeltaFlag) <
+					bDeltaFrag + bDeltaFlag + Math.abs(bDeltaFrag - bDeltaFlag);
+		}
+
+		let totalIter = 0;
+		let stagnantIter = 0;
+
+		while (stagnantIter < 6 && totalIter < 30) {
+			let old = _.cloneDeep(teams);
+			swapRandom();
+
+			if (compareTeamPairs(teams, old) == 0) {
+				stagnantIter++;
+				teams = old;
+			} else {
+				stagnantIter = 0;
+			}
+			totalIter++;
+		}
+
+		return { good: _.map(teams[0].players, 'name'), evil: _.map(teams[1].players, 'name') };
+	});
+}
+
+app.get('/api/players/teams', function(req, res) {
+	makeTeams(req.query['names'].split(' '))
+		.then(teams => { res.send(teams); })
+		.catch(err => { res.status(500).send({ error: err.message }); });
+});
+
+export function startTeamBalanceServer() {
+	const server = dgram.createSocket('udp4');
+
+	server.on('error', (err) => {
+		console.log(`server error:\n${err.stack}`);
+		server.close();
+	});
+
+	server.on('message', (data, rinfo) => {
+		let st = new Packet(data, 0);
+		let s;
+		let names = [];
+		while (s = st.getString()) {
+			names.push(s);
+		}
+
+		makeTeams(names)
+			.then(teams => {
+				let buf = new Buffer.alloc(1024);
+				let p = new Packet(buf);
+
+				_.each(teams, team => {
+					_.each(team, player => {
+						p.putInt(names.indexOf(player));
+					});
+				});
+
+				server.send(buf, 0, p.offset, rinfo.port, rinfo.address);
+			});
+	});
+
+	server.on('listening', () => {
+		var address = server.address();
+		console.log(`Team balance server listening on port ${address.port}`);
+	});
+
+	server.bind(config.teamBalanceServerPort);
+}
--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import database from './util/database';
 import serverManager from './tracker/server-manager';
 import cache from './util/cache';
 import playerManager from './tracker/player-manager';
+import {startTeamBalanceServer} from './api/v1/players.js';
 
 serverManager.start();
 cache.start();
 playerManager.start();
+startTeamBalanceServer();
 
 require('./util/admin');
 require('./web/main');
