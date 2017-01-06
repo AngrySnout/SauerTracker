3c260fb87e1ed9144d4e376084fc0581184fdbb7
AngrySnout <hreudontdy@gmail.com>
2017-01-06 18:48:54 +0200

    Fixed time format.

--- website/src/js/main.js
+++ website/src/js/main.js
@@ -14,7 +14,7 @@ window.url = url;
 window.dateReflow = function() {
     var now = moment();
     $(".date").each(function (ind, elem) {
-        var cd = moment($(this).text()+" +0000"); //, "YYYY-MM-DD HH:mm:ss Z"
+        var cd = moment($(this).text().replace(/[TZ]/g, " ").trim()+"+0000");
         $(this).attr("title", cd.format("dddd, MMMM Do YYYY, HH:mm:ss"));
         if (cd.diff(now, 'hours') >= -22) $(this).text(cd.fromNow());
         else if (cd.diff(now, 'days') >= -7) $(this).text(cd.fromNow()+cd.format(", HH:mm"));
