18319763d2398e3bc51d9474e8a05b189a2f6a4e
AngrySnout <origin@kussa.pw>
2017-09-10 03:48:17 +0300

    Part of "Show game stats in player page"

--- src/web/player.js
+++ src/web/player.js
@@ -8,7 +8,7 @@ import {getPlayer} from '../api/v1/player';
 
 app.get("/player/:name", function(req, res) {
 	getPlayer(req.params.name)
-		.then(result => { res.render("player", _.assign(result, { _: _, bannerURL: vars.bannerURL })); })
+		.then(result => { res.render("player", _.assign(result, { _: _, vars: vars })); })
 		.catch(ObjectNotFoundError, () => { res.status(404).render("error", { status: 404, error: "No player found with this name." }) })
 		.catch(ObjectBannedError, () => { res.status(400).render("error", { status: 400, error: "This player name is banned." }) })
 		.catch(err => { res.status(500).render("error", { status: 500, error: err.message }) });
