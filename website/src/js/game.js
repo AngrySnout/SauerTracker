13e3ba11e7a2303dbceadd9b76cf5c9080c1291e
AngrySnout <origin@kussa.pw>
2017-09-28 02:31:33 +0300

    Add similar games section to duel and clanwar game pages

--- website/src/js/game.js
+++ website/src/js/game.js
@@ -1,6 +1,21 @@
-import {loadCharts} from "./_server-charts.js";
+var $ = window.$;
 
-var addr = $("#server-address").text().split(":");
+var similarGamesTemplate = require('../views/_partials/similar-games.pug');
 
-window.tryLoadBackground($("#map-name").text());
-loadCharts(addr[0], addr[1]);
+var mapName = $("#map-name").text();
+var gameType = $("#game-type").text();
+
+window.tryLoadBackground(mapName);
+
+function loadSimilarGames() {
+    if (gameType === "duel" || gameType === "clanwar") {
+        var meta = JSON.parse($("#game-meta").text());
+        var query = "/games/find?gametype="+gameType+"&limit=10&players="+meta[0]+" "+meta[2];
+        $.get("/api"+query, function(result) {
+            $("#similar-games").html(similarGamesTemplate({ similarGames: result.results, viewAllLink: query }));
+            $("#similar-games-parent").css("display", "block");
+            disableDefault();
+        });
+    }
+}
+loadSimilarGames();
