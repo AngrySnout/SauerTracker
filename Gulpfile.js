0a6c2dc8ac747422eeab5d9039f669d80cdeb8bd
AngrySnout <origin@kussa.pw>
2017-12-09 03:09:22 +0200

    Add gulp-plumber and redis

--- Gulpfile.js
+++ Gulpfile.js
@@ -15,6 +15,7 @@ var buffer = require('vinyl-buffer');
 var glob = require('glob');
 var gulpif = require('gulp-if');
 var rename = require("gulp-rename");
+var plumber = require('gulp-plumber');
 
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
@@ -28,88 +29,83 @@ var uglify = require('gulp-uglify');
 var dev = (process.env.NODE_ENV !== "production");
 
 gulp.task('sass', function() {
-    return gulp.src('./website/styles/*.scss')
-    .pipe(gulpif(dev, sourcemaps.init()))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulpif(!dev, cleanCSS()))
-    .pipe(gulpif(dev, sourcemaps.write('./')))
-    .pipe(gulp.dest('./assets/styles'));
+	return gulp.src('./website/styles/*.scss')
+		.pipe(plumber())
+		.pipe(gulpif(dev, sourcemaps.init()))
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulpif(!dev, cleanCSS()))
+		.pipe(gulpif(dev, sourcemaps.write('./')))
+		.pipe(gulp.dest('./assets/styles'));
 });
 
 function bundleJS(file, watch) {
-    var bundler;
-    var args = { cache: {}, packageCache: {}, entries: [file], debug: true };
+	var bundler;
+	var args = { cache: {}, packageCache: {}, entries: [file], debug: true };
 
-    if (watch) bundler = watchify(browserify(args));
-    else bundler = browserify(args);
+	if (watch) bundler = watchify(browserify(args));
+	else bundler = browserify(args);
 
-    bundler.transform('babelify', {presets: ['es2015'], only: /\/website\/js/})
-        .transform('browserify-shim')
-        .transform('debowerify')
-        .transform('./jadeify');
+	bundler.transform('babelify', {presets: ['es2015'], only: /\/website\/js/})
+		.transform('browserify-shim')
+		.transform('debowerify')
+		.transform('./jadeify');
 
-    function bundle() {
-        bundler.bundle()
-            .on('error', function (err) { gutil.log(err.message); })
-            .pipe(source(file))
-            .pipe(buffer())
-            .pipe(gulpif(!dev, uglify()))
-                .on('error', function(e){
-                    console.log(e);
-                })
-            .pipe(rename(path => { path.dirname = ''; }))
-            .pipe(gulp.dest('./assets/js/'));
-    }
-    bundle();
+	function bundle() {
+		bundler.bundle()
+			.pipe(plumber())
+			.pipe(source(file))
+			.pipe(buffer())
+			.pipe(gulpif(!dev, uglify()))
+			.pipe(rename(path => { path.dirname = ''; }))
+			.pipe(gulp.dest('./assets/js/'));
+	}
+	bundle();
 
-    if (watch) bundler.on('update', function () { bundle(); });
+	if (watch) bundler.on('update', function () { bundle(); });
 }
 
 function makeBundles(watch) {
-    glob('./website/js/**/[^_]*.js', function(err, files) {
-        if(err) return gutil.log(err);
-        files.map(function(entry) {
-            bundleJS(entry, watch);
-        });
-    });
+	glob('./website/js/**/[^_]*.js', function(err, files) {
+		if(err) return gutil.log(err);
+		files.map(function(entry) {
+			bundleJS(entry, watch);
+		});
+	});
 }
 
 gulp.task('js', function() {
-    makeBundles();
-    return gulp.src('./src/**/*')
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(babel({presets: ['es2015']}))
-            .on('error', function(e) {
-                console.log(e);
-                this.emit('end');
-            })
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./build/'));
+	makeBundles();
+	return gulp.src('./src/**/*')
+		.pipe(plumber())
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(babel({presets: ['es2015']}))
+			.on('error', function(e) {
+				console.log(e);
+				this.emit('end');
+			})
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./build/'));
 });
 
 gulp.task('clean', function() {
-    return del(['./assets/js/**/*', './assets/styles/**/*']);
+	return del(['./assets/js/**/*', './assets/styles/**/*']);
 });
 
 gulp.task('watch', function() {
 	gulp.watch('./website/styles/*.scss', ['sass']);
 	gulp.watch('./website/assets/**/*', ['static']);
-    makeBundles(true);
-    return gulp.src('src/**/*')
-            .on('error', console.log)
-        .pipe(watch('src/**/*'))
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(babel({presets: ['es2015']}))
-            .on('error', function(e) {
-                console.log(e);
-                this.emit('end');
-            })
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./build/'));
+	makeBundles(true);
+	return gulp.src('src/**/*')
+		.pipe(plumber())
+		.pipe(watch('src/**/*'))
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(babel({presets: ['es2015']}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./build/'));
 });
 
 gulp.task('build', ['sass', 'js']);
