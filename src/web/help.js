4c8a43ca380fb2cd69b48fd97a6f243f52ce7da0
AngrySnout <splintersal@gmail.com>
2018-02-18 01:00:42 +0200

    Remove API documentation

--- src/web/help.js
+++ src/web/help.js
@@ -1,10 +1,5 @@
 import app from '../util/web';
-import apiPaths from './paths';
 
 app.get('/faq', (req, res) => {
 	res.render('faq');
-});
-
-app.get('/api', (req, res) => {
-	res.render('api', { paths: apiPaths });
 });
